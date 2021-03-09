bdb182e75680697334d42ee3fe4e9618ae921f23
MaximeJoeger <76443502+MaximeJoeger@users.noreply.github.com>
2021-03-09 18:33:20 +0100

    Update my_compute_power_rec.c

--- Jour5/my_compute_power_rec.c
+++ Jour5/my_compute_power_rec.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdlib.h>
 
 int my_compute_power_rec(int nb, int p)
 {
@@ -10,16 +9,4 @@ int my_compute_power_rec(int nb, int p)
   else if (p < 0)
     nb = 0;
   return nb;
-}
-int main()
-{
-  int nb;
-
-  nb = my_compute_power_rec(4, 5);
-  int nb1 = my_compute_power_rec(4, 0);
-  int nb2 = my_compute_power_rec(4, -8);
-  printf("%d\n", nb);
-  printf("%d\n", nb1);
-  printf("%d\n", nb2);
-  return 0;
 }
