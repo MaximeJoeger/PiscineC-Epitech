5b7ee55a3fc1349347353b1af483d4688be832f5
MaximeJoeger <76443502+MaximeJoeger@users.noreply.github.com>
2021-03-09 18:30:07 +0100

    Update my_swap.c

--- Jour4/my_swap.c
+++ Jour4/my_swap.c
@@ -1,21 +1,8 @@
 #include <unistd.h>
-#include <stdio.h>
 
 void my_swap(int *a,int *b)
 {
   int saveA = *a;
   *a = *b;
   *b = saveA;
-}
-int main()
-{
-  int a;
-  int b;
-
-  a = 8;
-  b = 3;
-  my_swap(&a, &b);
-  printf("%d, %d\n", a, b);
-
-  return 0;
 }
